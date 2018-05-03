8117e7ab1784f210df175e17465ab69b9a5fe528
Tobias Fluck <tobi-fl@hotmail.de>
2018-05-03 11:00:56 +0200

    Updated

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+var clients = 0;
+io.on('connection', function(socket) {
+   clients++;
+   io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+   socket.on('disconnect', function () {
+      clients--;
+      io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
 });
